07f89a28fcc26602b7dc6496860846809bf1f447
Aditya Arora <adityadvlp@gmail.com>
2018-01-11 01:29:27 +0530

    Adding to-categorical updating model using new loss

--- simple_dnn_model.py
+++ simple_dnn_model.py
@@ -6,8 +6,11 @@ Created on Thu Jan 11 00:52:35 2018
 """
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense,Dropout
 from sklearn.metrics import accuracy_score
+
+
+from keras.utils import to_categorical
 
 import numpy as np
 np.random.seed(1234)
@@ -17,15 +20,35 @@ train_x=np.load('train_x.npy')
 train_y=np.load('train_y.npy')
 #test_x=np.load('test_x.npy')
 #test_y=np.load('test_y.npy')
+input_neurons=200
+input_dim=3072
+act1='sigmoid'
+act2='sigmoid'
+act3='sigmoid'
+act4='sigmoid'
+num_classes=10
+dropout=0.2
 
 model = Sequential()
-model.add(Dense(200, input_dim=3072, init='uniform', activation='relu'))
-model.add(Dense(200, init='uniform', activation='relu'))
-model.add(Dense(1, init='uniform', activation='sigmoid'))
-# Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(Dense(input_neurons, input_dim = input_dim, activation=act1))
+model.add(Dropout(dropout))
+model.add(Dense(input_neurons, activation=act2))
+model.add(Dropout(dropout))
+model.add(Dense(input_neurons, activation=act3))
+model.add(Dropout(dropout))
+model.add(Dense(num_classes, activation=act4))
+#model.summary()
+model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+   
+
+train_y=to_categorical(train_y, num_classes=None)
+ 
 # Fit the model
 model.fit(train_x, train_y, epochs=10, batch_size=32,  verbose=2)
+
+
 
 """
 # calculate predictions
