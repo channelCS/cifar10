f56e0c340b79f744ffea80b00c153f9fdafff420
Aditya Arora <adityadvlp@gmail.com>
2018-01-11 01:17:43 +0530

    Adding accuracy score

--- simple_dnn_model.py
+++ simple_dnn_model.py
@@ -7,13 +7,16 @@ Created on Thu Jan 11 00:52:35 2018
 
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.metrics import accuracy_score
 
 import numpy as np
 np.random.seed(1234)
 
 
-X=np.load('train_x.npy')
-Y=np.load('train_y.npy')
+train_x=np.load('train_x.npy')
+train_y=np.load('train_y.npy')
+#test_x=np.load('test_x.npy')
+#test_y=np.load('test_y.npy')
 
 model = Sequential()
 model.add(Dense(200, input_dim=3072, init='uniform', activation='relu'))
@@ -22,9 +25,12 @@ model.add(Dense(1, init='uniform', activation='sigmoid'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
-model.fit(X, Y, epochs=10, batch_size=32,  verbose=2)
+model.fit(train_x, train_y, epochs=10, batch_size=32,  verbose=2)
+
+"""
 # calculate predictions
-predictions = model.predict(X)
+y_pred = model.predict(test_x)
 # round predictions
-rounded = [round(x[0]) for x in predictions]
-print(rounded)
+acc2=accuracy_score(test_y, y_pred)
+print "Accuracy",acc2
+"""
